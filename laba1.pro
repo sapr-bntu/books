99440ba4fd993f8317f351343a912a57cfa46b12
ferz <s5005a@gmail.com>
2011-10-14 15:17:29 +0300

    first commit

---
+++ laba1.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-09-21T19:12:25
+#
+#-------------------------------------------------
+
+QT       += core gui sql
+
+TARGET = laba1
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+    conn.h \
+    mainwindow.h
+
+FORMS    += mainwindow.ui
+
+OTHER_FILES +=
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSql>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSqlTableModel *model;
+
+
+private slots:
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_clicked(bool checked);
+    void on_pushButton_pressed();
+    void on_pushButton_clicked();
+};
+
+#endif // MAINWINDOW_H
